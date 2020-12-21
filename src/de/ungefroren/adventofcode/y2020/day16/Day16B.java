4ec607dbe3f0a9bb87fcfcf4402d7b9e3ff7b9f7
Jonas <joblo.2213@gmail.com>
2020-12-21 15:03:28 +0100

    Use cleanup algorithm from day 21 b to solve day 16 b

--- src/de/ungefroren/adventofcode/y2020/day16/Day16B.java
+++ src/de/ungefroren/adventofcode/y2020/day16/Day16B.java
@@ -4,8 +4,9 @@ import de.ungefroren.adventofcode.y2020.helpers.PuzzleInput;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -20,18 +21,36 @@ public class Day16B {
                 .map(Ticket::from).collect(Collectors.toList());
         List<Ticket> valid = nearby.stream().filter(t -> t.checkValidity(rules)).collect(Collectors.toList());
 
+        Map<Rule, Set<Integer>> indices = new HashMap<>();
+        rules.forEach(rule -> {
+            Set<Integer> is = IntStream.range(0, myTicket.values.size())
+                    .filter(i -> valid.stream().allMatch(t -> rule.isValid(t.values.get(i))))
+                    .boxed()
+                    .collect(Collectors.toSet());
+            indices.put(rule, is);
+        });
+
+        int knownSize = 0;
+        Map<Rule, Integer> known;
+        while (true) {
+            known = indices.entrySet().stream().filter(e -> e.getValue().size() == 1)
+                    .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().iterator().next()));
+            if (knownSize == known.size()) break;
+            knownSize = known.size();
+            known.forEach((rule, index) -> indices.forEach((r, i) -> {
+                if (!r.equals(rule)) i.remove(index);
+            }));
+        }
+        if (known.size() != indices.size()) throw new RuntimeException("not all indices are known");
         long val = 1;
-        for (Rule rule : rules/*.stream().filter(r -> r.lbl.startsWith("departure ")).collect(Collectors.toList())*/) {
-            int index = IntStream.range(0, myTicket.values.size())
-                    .filter(i ->
-                            valid.stream().allMatch(t -> rule.isValid(t.values.get(i)))
-                    ).findAny().getAsInt();
+        for (Map.Entry<Rule, Integer> entry : known.entrySet()) {
+            Rule rule = entry.getKey();
+            Integer index = entry.getValue();
             if (rule.lbl.startsWith("departure ")) {
                 val *= myTicket.values.get(index);
             }
             System.out.println(rule.lbl + ": " + myTicket.values.get(index));
         }
         System.out.println(val);
-        //FIXME do not use same value twice
     }
 }
